51e6d7b16251ad41ca9a29e0c75ad087c9084a31
singhsduos <neelesh.singh@jalantechnologies.com>
2022-06-06 01:32:49 +0530

    created - repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alphabet } from "./page/alphabet.component";
+import Provider from "./context/provider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider>
+        <Alphabet />
+      </Provider>
+    </>
   );
 }
 
